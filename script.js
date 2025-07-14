44a34729431e56374537c2161be67265fb8a9ff9
Anamika Anand <itzanamikaanand@gmail.com>
2025-07-14 23:00:17 +0530

    Alarm App
     Alarm App – Built with HTML, CSS & JavaScript
    This Alarm App is a lightweight and interactive web application created using HTML, CSS, and JavaScript. It allows users to set and manage alarms directly in the browser, featuring real-time time tracking and responsive user interface design. The project demonstrates core JavaScript skills like setInterval, event handling, and DOM manipulation without any external libraries. It's a perfect beginner-to-intermediate level project to strengthen your front-end development skills and is mobile-friendly as well.
    
    #JavaScript #WebDevelopment #FrontendDeveloper #AlarmApp #100DaysOfCode #HTML5 #CSS3 #VanillaJS #ResponsiveDesign #CodeNewbie #LearnToCode #DevCommunity #WomenWhoCode #JSProjects #OpenSource #TechInnovation

--- script.js
+++ script.js
@@ -1,41 +1,141 @@
-const billInput = document.getElementById("bill");
-const tipInput = document.getElementById("tip");
-const noOfPeopleInput = document.getElementById("no-of-people");
-const tipAmountOutput = document.getElementById("tip-amount");
-const totalAmountOutput = document.getElementById("total-amount");
-const tipPercentOutput = document.getElementById("tip-percent");
-const splitNumOutput = document.getElementById("split-num");
-const tipPerPersonOutput = document.getElementById("tip-per-person");
-const totalPerPersonOutput = document.getElementById("total-per-person");
-
-const sliders = document.querySelectorAll("input[type='range']");
-
-sliders.forEach(function (slider) {
-  slider.addEventListener("input", calculateTip);
-});
-
-billInput.addEventListener("input", calculateTip);
-tipInput.addEventListener("input", calculateTip);
-noOfPeopleInput.addEventListener("input", calculateTip);
-
-function calculateTip() {
-  const bill = parseFloat(billInput.value);
-  const tipPercent = parseFloat(tipInput.value);
-  const noOfPeople = parseInt(noOfPeopleInput.value);
-
-  const totalTip = (bill * (tipPercent / 100)).toFixed(2);
-  const total = (bill + parseFloat(totalTip)).toFixed(2);
-  const tipPerPerson = (totalTip / noOfPeople).toFixed(2);
-  const totalPerPerson = (total / noOfPeople).toFixed(2);
-
-  billInput.value = bill.toFixed(2);
-
-  tipAmountOutput.textContent = `${totalTip} €`;
-  totalAmountOutput.textContent = `${total} €`;
-  tipPercentOutput.textContent = `${tipPercent}%`;
-  splitNumOutput.textContent = noOfPeople;
-  tipPerPersonOutput.textContent = `${tipPerPerson} €`;
-  totalPerPersonOutput.textContent = `${totalPerPerson} €`;
+//Initial References
+let timerRef = document.querySelector(".timer");
+const hourInput = document.getElementById("hourInput");
+const minuteInput = document.getElementById("minuteInput");
+const activeAlarms = document.querySelector(".activeAlarms");
+const setAlarm = document.getElementById("set");
+let alarmsArray = [];
+let alarmSound = new Audio(
+  "https://assets.mixkit.co/active_storage/sfx/1003/1003-preview.mp3"
+);
+let initialHour = 0,
+  initialMinute = 0,
+  alarmIndex = 0;
+//Append zeroes for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
+//Search for value in object
+const searchObject = (parameter, value) => {
+  let alarmObject,
+    objIndex,
+    exists = false;
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm[parameter] == value) {
+      exists = true;
+      alarmObject = alarm;
+      objIndex = index;
+      return false;
+    }
+  });
+  return [exists, alarmObject, objIndex];
+};
+//Display Time
+function displayTimer() {
+  let date = new Date();
+  let [hours, minutes, seconds] = [
+    appendZero(date.getHours()),
+    appendZero(date.getMinutes()),
+    appendZero(date.getSeconds())
+  ];
+  //Display time
+  timerRef.innerHTML = `${hours}:${minutes}:${seconds}`;
+  //Alarm
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm.isActive) {
+      if (`${alarm.alarmHour}:${alarm.alarmMinute}` === `${hours}:${minutes}`) {
+        alarmSound.play();
+        alarmSound.loop = true;
+      }
+    }
+  });
 }
-
-calculateTip();
+const inputCheck = (inputValue) => {
+  inputValue = parseInt(inputValue);
+  if (inputValue < 10) {
+    inputValue = appendZero(inputValue);
+  }
+  return inputValue;
+};
+hourInput.addEventListener("input", () => {
+  hourInput.value = inputCheck(hourInput.value);
+});
+minuteInput.addEventListener("input", () => {
+  minuteInput.value = inputCheck(minuteInput.value);
+});
+//Create alarm div
+const createAlarm = (alarmObj) => {
+  //Keys from object
+  const { id, alarmHour, alarmMinute } = alarmObj;
+  //Alarm div
+  let alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarm__item");
+  alarmDiv.setAttribute("data-id", id);
+  alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}</span>`;
+  //checkbox
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      startAlarm(e);
+    } else {
+      stopAlarm(e);
+    }
+  });
+  alarmDiv.appendChild(checkbox);
+  //Delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+  alarmDiv.appendChild(deleteButton);
+  activeAlarms.appendChild(alarmDiv);
+};
+//Set Alarm
+setAlarm.addEventListener("click", () => {
+  alarmIndex += 1;
+  //alarmObject
+  let alarmObj = {};
+  alarmObj.id = `${alarmIndex}_${hourInput.value}_${minuteInput.value}`;
+  alarmObj.alarmHour = hourInput.value;
+  alarmObj.alarmMinute = minuteInput.value;
+  alarmObj.isActive = false;
+  console.log(alarmObj);
+  alarmsArray.push(alarmObj);
+  createAlarm(alarmObj);
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+});
+//Start Alarm
+const startAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = true;
+  }
+};
+//Stop alarm
+const stopAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = false;
+    alarmSound.pause();
+  }
+};
+//delete alarm
+const deleteAlarm = (e) => {
+  let searchId = e.target.parentElement.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    e.target.parentElement.parentElement.remove();
+    alarmsArray.splice(index, 1);
+  }
+};
+window.onload = () => {
+  setInterval(displayTimer);
+  initialHour = 0;
+  initialMinute = 0;
+  alarmIndex = 0;
+  alarmsArray = [];
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+};
